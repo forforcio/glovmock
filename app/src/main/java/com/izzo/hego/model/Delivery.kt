558bae6764dacac285dda073c7ddc4efa967bc30
Enrique Ortega <enrique.ortega.cabello@proton.me>
2026-03-30 16:17:37 +0200

    first commit

---
+++ app/src/main/java/com/izzo/hego/model/Delivery.kt
@@ -0,0 +1,9 @@
+package com.izzo.hego.model
+
+data class Delivery(
+    val id: String,
+    val restaurantName: String,
+    val restaurantAddress: String,
+    val restaurantType: String,
+)
+
---
+++ app/src/main/java/com/izzo/hego/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.izzo.hego.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Orange = Color(0xFFE76215)
+
+val LightOrange = Color(0xFFFFA366)
+
+val LighterOrange = Color(0xFFFFC9B3)
+
+val OrangyWhite = Color(0xFFFFF2E6)
